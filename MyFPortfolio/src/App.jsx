b500304d23f3c55a4238ffa9bc6db02bac9fe1bf
niraj715 <nirajisgreathero@gmail.com>
2025-03-21 09:13:50 +0545

    BrowseRouter setupped

--- MyFPortfolio/src/App.jsx
+++ MyFPortfolio/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from './Component/Header'
 
 function App() {
   return (
     <div>
-      
+      <Header/>
     </div>
   )
 }
---
+++ MyFPortfolio/src/Component/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Routes } from 'react-router-dom'
+
+function Header() {
+  return (
+    <>
+      <h1>This is Header Section</h1>
+      <Routes>
+        
+      </Routes>
+    </>
+  )
+}
+
+export default Header
