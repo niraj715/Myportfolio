59093bf5245cee62aaa930ada2cd0c258fb463d3
niraj715 <nirajisgreathero@gmail.com>
2025-03-21 23:18:38 +0545

    modifoed homepage

--- MyFPortfolio/src/Component/Header.jsx
+++ MyFPortfolio/src/Component/Header.jsx
@@ -4,12 +4,20 @@ import Home from './Home'
 import About from './About'
 
 function Header() {
+  $(window).scroll(() => {
+    if ($(window).scrollTop() > 500) {
+        $('.head1').css('opacity', '0');  // Hide header
+        $('.head2').fadeIn();  // Show sticky header
+    } else {
+        $('header').css('opacity', '1');  // Show header
+        $('.fixed').fadeOut();  // Hide sticky header
+    }
+});
   return (
     <>
     <header>
-<div>
- 
-  <nav id="navbar-example2" className="navbar bg-body-transparent px-3 mb-3 ">
+   <div className='head1'>
+    <nav id="navbar-example2" className="navbar px-3 mb-3 ">
     <a className="navbar-brand" href="#"> <span className='logo'>Yor_Niraj</span></a>
     <ul className="nav nav-pills d-flex gap-2">
       <li className="nav-item">
@@ -40,8 +48,40 @@ function Header() {
     </ul>
     
   </nav>
- 
-</div>
+  </div>
+  <div className="head2">
+  <nav id="navbar-example2" className="navbar px-3 mb-3 ">
+    <a className="navbar-brand" href="#"> <span className='logo'>Yor_Niraj</span></a>
+    <ul className="nav nav-pills d-flex gap-2">
+      <li className="nav-item">
+        <Link className="nav-link" to="/">Home</Link>
+      </li>
+      <li className="nav-item">
+        <Link className="nav-link" to="/about">About</Link>
+      </li>
+      <li className="nav-item">
+        <Link className="nav-link" to="/about">Services</Link>
+      </li>
+      <li className="nav-item">
+        <Link className="nav-link" to="/about">Blogs</Link>
+      </li>
+      <li className="nav-item">
+        <Link className="nav-link" to="/about">My CV </Link>
+      </li>
+      <li className="nav-item dropdown">
+        <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Contact </a>
+        <ul className="dropdown-menu">
+          <li><a className="dropdown-item" href="#scrollspyHeading3">Facebook</a></li>
+          <li><a className="dropdown-item" href="#scrollspyHeading4">Linkedin</a></li>
+          
+          <li><a className="dropdown-item" href="#scrollspyHeading5"></a></li>
+        </ul>
+      </li>
+     
+    </ul>
+    
+  </nav>
+  </div>
 
 
    <Routes>
